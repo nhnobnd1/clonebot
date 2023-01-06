2def1b919f71283fd4ab3315587ef360ea4d494f
nhnobnd1 <89453030+nhnobnd1@users.noreply.github.com>
2023-01-07 05:28:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
     TG_USER_SESSION = os.environ.get("postgresql://wzyjhhfgfoynfblbhghsfczc%40psql-mock-database-cloud:ylhshiivggwcauldbzaizwec@psql-mock-database-cloud.postgres.database.azure.com:5432/booking1673029584132dgnjnarubhaohjzd")
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "postgres://cnsehumnouvtlr:2d72e47d2ef2114f586fc9345a2dd04bffb2afb9583f4a481493057aa70c0d35@ec2-35-153-35-94.compute-1.amazonaws.com:5432/dd9pfob30m977q")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://postgres:hnrLHo36uM5yt4l@clone.internal:5432")
 
 
 def LOGGER(name: str) -> logging.Logger:
